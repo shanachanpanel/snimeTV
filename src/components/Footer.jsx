4c505c287788f32aeed04527bd9bc781cf6064a4
shanachan <77118749+kap320@users.noreply.github.com>
2023-12-18 15:00:07 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
             <div className="d-flex justify-content-center">
                 <hr style={{width:'40%'}}/>
             </div>
-            <a className="contact" style={{textDecoration:'none',color:'white'}} href="https://github.com/silentm4gician" target="_blank"> silentM4gician </a>
+            <a className="contact" style={{textDecoration:'none',color:'white'}} href="https://github.com/silentm4gician" target="_blank"> shallownimeTV </a>
             | 
-            <a className="contact" style={{textDecoration:'none',color:'white'}}href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=leandroGonzalezMat@gmail.com" target="_blank" > contact </a>
-            | ©2023
+            <a className="contact" style={{textDecoration:'none',color:'white'}}href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=rizaedtz@gmail.com" target="_blank" > contact </a>
+            | ©2024
         </footer>
     )
 }
