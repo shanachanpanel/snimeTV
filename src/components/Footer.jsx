0024db5000983eb34cfda64bdde1c4c428054c4d
shanachan <77118749+kap320@users.noreply.github.com>
2023-12-18 15:04:05 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
             <div className="d-flex justify-content-center">
                 <hr style={{width:'40%'}}/>
             </div>
-            <a className="contact" style={{textDecoration:'none',color:'white'}} href="https://github.com/silentm4gician" target="_blank"> shallownimeTV </a>
+            <a className="contact" style={{textDecoration:'none',color:'white'}} href="https://portofolio.ahmadriza.repl.co" target="_blank"> shallownimeTV </a>
             | 
             <a className="contact" style={{textDecoration:'none',color:'white'}}href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=rizaedtz@gmail.com" target="_blank" > contact </a>
             | ©2024
